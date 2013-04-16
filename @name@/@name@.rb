eecbc4dd08128980b12ca64e20999324abf76389
AjayMT <ajay.tatachar@gmail.com>
2013-04-16 09:58:15 +0530

    Minor change.

--- @name@/@name@.rb
+++ @name@/@name@.rb
@@ -7,6 +7,5 @@ client = MongoClient.new('localhost', 27017)
 db = client.db('@name@db')
 
 get '/' do
-  'Hello, Sinatra and MongoDB!<br />' +
-  'Welcome to @name@.'
+  'Hello, Sinatra and MongoDB!'
 end
